0009b46a08fc9776fae3cba716e5147e4fe9d2a3
toramameseven <toramameseven@gmail.com>
2025-09-24 08:07:51 +0900

    rename RESXProcessor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // LICENSE : MIT
 "use strict";
-import HTMLProcessor from "./HTMLProcessor.js";
+import HTMLProcessor from "./RESXProcessor.js";
 import type { TextlintPluginCreator } from "@textlint/types";
 
 export default {
